07da57b52b8596446765f251e195bbe180699190
ZhengChengkai826 <zchengkai826@gmail.com>
2019-05-19 04:30:10 -0700

    add func to detail buttons

--- ZCKFinal/DetailViewController.swift
+++ ZCKFinal/DetailViewController.swift
@@ -17,6 +17,7 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var hometown: UITextField!
     @IBOutlet weak var phone: UITextField!
     @IBOutlet weak var email: UITextField!
+    private var student: Student?;
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -41,18 +42,48 @@ class DetailViewController: UIViewController {
             return
         }
         
-        let student = fetchedResultsController.object(at: IndexPath(item: 0, section: 0))
-        name.text = student.name
-        id.text = String(student.id)
-        school.text = student.school
-        hometown.text = student.hometown
-        phone.text = String(student.phone)
-        email.text = student.email
+        student = fetchedResultsController.object(at: IndexPath(item: 0, section: 0))
+        name.text = student?.name
+        id.text = String(student!.id)
+        school.text = student?.school
+        hometown.text = student?.hometown
+        phone.text = String(student!.phone)
+        email.text = student?.email
         
         // Do any additional setup after loading the view.
     }
     
 
+    @IBOutlet weak var modifyButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBAction func modifyThis(_ sender: UIButton) {
+        if modifyButton.title(for: .normal) == "修改" {
+            id.isEnabled = true
+            school.isEnabled = true
+            hometown.isEnabled = true
+            phone.isEnabled = true
+            email.isEnabled = true
+            modifyButton.setTitle("提交", for: .normal)
+            deleteButton.isEnabled = false
+        } else {
+            id.isEnabled = false
+            school.isEnabled = false
+            hometown.isEnabled = false
+            phone.isEnabled = false
+            email.isEnabled = false
+            modifyButton.setTitle("修改", for: .normal)
+            deleteButton.isEnabled = true
+        }
+    }
+    
+    @IBAction func deleteThis(_ sender: UIButton) {
+        AppDelegate.viewContext.delete(student!)
+        if let vctlers = navigationController?.viewControllers {
+            let parent = vctlers[vctlers.count-2] as? InfoManViewController
+            parent?.updateFetchResults()
+        }
+        navigationController?.popViewController(animated: true)
+    }
     /*
     // MARK: - Navigation
 
