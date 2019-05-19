c5a76c02e1380a3445f774a97c4695777e81a831
ZhengChengkai826 <zchengkai826@gmail.com>
2019-05-19 05:06:46 -0700

    finish ui & model

---
+++ ZCKFinal/Account.swift
@@ -0,0 +1,14 @@
+//
+//  Account.swift
+//  ZCKFinal
+//
+//  Created by iqra on 2019/5/19.
+//  Copyright © 2019 iqra. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Account: NSManagedObject {
+
+}
---
+++ ZCKFinal/Score.swift
@@ -0,0 +1,14 @@
+//
+//  Score.swift
+//  ZCKFinal
+//
+//  Created by iqra on 2019/5/19.
+//  Copyright © 2019 iqra. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Score: NSManagedObject {
+
+}
