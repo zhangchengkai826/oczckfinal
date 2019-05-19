589084cf081ae96f41c26514da00317afb7ab769
ZhengChengkai826 <zchengkai826@gmail.com>
2019-05-19 05:51:31 -0700

    finish all

--- ZCKFinal/LoginViewController.swift
+++ ZCKFinal/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class LoginViewController: UIViewController {
 
@@ -16,7 +17,47 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var passwd: UITextField!
+    @IBAction func login(_ sender: UIButton) {
+        if username.text == nil || passwd.text == nil {
+            return
+        }
+        
+        let context = AppDelegate.viewContext
+        
+        let request: NSFetchRequest<Account> = Account.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(
+            key: "username", ascending: true,
+            selector: #selector(NSString.localizedStandardCompare(_:))
+            )]
+        request.predicate = NSPredicate(format: "username == %@", username.text!)
+        let fetchedResultsController = NSFetchedResultsController<Account>(
+            fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: "account")
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            return
+        }
+        
+        if fetchedResultsController.fetchedObjects?.count == 0 {
+            let alert = UIAlertController(title: "Error", message: "用户名不存在", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "重试", style: .default))
+            self.present(alert, animated: true)
+            return
+        }
+        let account = fetchedResultsController.object(at: IndexPath(item: 0, section: 0))
+        if account.passwd == passwd.text {
+            let alert = UIAlertController(title: "Succeed", message: "登录成功", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(alert, animated: true)
+        } else {
+            let alert = UIAlertController(title: "Error", message: "用户名或密码错误", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "重试", style: .default))
+            self.present(alert, animated: true)
+        }
+    }
     /*
     // MARK: - Navigation
 
--- ZCKFinal/ScoreFetchViewController.swift
+++ ZCKFinal/ScoreFetchViewController.swift
@@ -7,16 +7,65 @@
 //
 
 import UIKit
+import CoreData
 
-class ScoreFetchViewController: UIViewController {
-
+class ScoreFetchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let cellIdentifier = "score"
+    var fetchedResultsController: NSFetchedResultsController<Score>?
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchInput: UITextField!
+    @IBAction func search(_ sender: UIButton) {
+        if searchInput.text == nil {
+            return
+        }
+        
+        let context = AppDelegate.viewContext
+        
+        let request: NSFetchRequest<Score> = Score.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(
+            key: "subject", ascending: true,
+            selector: #selector(NSString.localizedStandardCompare(_:))
+            )]
+        request.predicate = NSPredicate(format: "student.name == %@", searchInput.text!)
+        fetchedResultsController = NSFetchedResultsController<Score>(
+            fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: "score")
+        try? fetchedResultsController?.performFetch()
+        
+        tableView.reloadData()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    func numberOfSectionsInTableView(sender: UITableView) -> Int {
+        return fetchedResultsController?.sections?.count ?? 1
+    }
+    
+    func tableView(_ sender: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let sections = fetchedResultsController?.sections, sections.count > 0 {
+            return sections[section].numberOfObjects
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if(cell == nil) {
+            cell = UITableViewCell(
+                style: UITableViewCell.CellStyle.default,
+                reuseIdentifier: cellIdentifier
+            )
+        }
+        if let obj = fetchedResultsController?.object(at: indexPath) {
+            cell?.textLabel?.text = obj.subject! + ": " + String(obj.score) + "分"
+        }
+        return cell!
+    }
+    
     /*
     // MARK: - Navigation
 
