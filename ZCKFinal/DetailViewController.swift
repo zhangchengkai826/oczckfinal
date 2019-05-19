96ffcee026049575d988ad5451ff4641d86ee105
ZhengChengkai826 <zchengkai826@gmail.com>
2019-05-19 02:14:34 -0700

    finish show detail v1

--- ZCKFinal/DetailViewController.swift
+++ ZCKFinal/DetailViewController.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var id: UITextField!
+    @IBOutlet weak var school: UITextField!
+    @IBOutlet weak var hometown: UITextField!
+    @IBOutlet weak var phone: UITextField!
+    @IBOutlet weak var email: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if navigationItem.title == nil {
+            return
+        }
 
+        let context = AppDelegate.viewContext
+        
+        let request: NSFetchRequest<Student> = Student.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(
+            key: "name", ascending: true,
+            selector: #selector(NSString.localizedStandardCompare(_:))
+            )]
+        request.predicate = NSPredicate(format: "name == %@", navigationItem.title!)
+        let fetchedResultsController = NSFetchedResultsController<Student>(
+            fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: "student")
+
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            return
+        }
+        
+        let student = fetchedResultsController.object(at: IndexPath(item: 0, section: 0))
+        name.text = student.name
+        id.text = String(student.id)
+        school.text = student.school
+        hometown.text = student.hometown
+        phone.text = String(student.phone)
+        email.text = student.email
+        
         // Do any additional setup after loading the view.
     }
     
